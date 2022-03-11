831b6ce03d554f79d5a50631924c4c6f719d7552
Lohan <lohanext@gmail.com>
2022-03-11 10:51:41 -0300

    fix Grid class
    corrections and refactoring on grid class

--- Grid.h
+++ Grid.h
@@ -10,12 +10,12 @@ public:
     Grid(int Lines, int Columns);
     ~Grid();
 
-    std::vector<Types::GridBox> grids;
+    std::vector<Types::GridBox*> gridBoxes;
 
     int xLenght;
     int yLength;
     
     // prints the matrix that indicates the tiles of the battlefield
-    void drawBattlefield(int Lines, int Columns);
+    void drawBattlefield();
 };
 
